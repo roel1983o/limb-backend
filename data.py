314769d3ba4ba9a46864b5e1a2fa186217f038c3
roel1983o <roel.ophelders@delimburger.nl>
2025-04-29 14:44:01 +0200

    Create data.py

---
+++ data.py
@@ -0,0 +1,9 @@
+TREFWOORDEN = [
+    "limburg", "limburger", "limburgse", "limburgsen",
+    "maastricht", "sittard", "geleen", "venlo", "venray",
+    "roermond", "weert", "kerkrade", "heerlen", "brunssum",
+    "parkstad", "landgraaf", "simpelveld", "voerendaal",
+    "gulpen", "vaals", "valkenburg", "eijsden", "margraten",
+    "meerssen", "schinnen", "susteren", "roerdalen",
+    "maasgouw", "leudal", "nederweert", "beesel", "reuver", "mook", "gennep"
+]
